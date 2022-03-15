3b3e9a6d2aa6a9278b5eb4b225192762432e8613
고동현 <dhjk35@naver.com>
2022-03-15 23:36:08 +0900

    Fail: [백준 Silver 5] 2751번 수 정렬하기 2 - 시간초과로 실패

---
+++ BOJ/Sort/2751_수 정렬하기 2/solve.js
@@ -0,0 +1,14 @@
+const [n, ...input] = require("fs")
+  // 백준 제출 시 './input.txt'를 '/dev/stdin'으로 변경
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => +v);
+
+function Solution(n, input) {
+  const answer = input.sort((a, b) => a - b);
+  answer.forEach((num) => console.log(num));
+}
+
+Solution(n, input);
