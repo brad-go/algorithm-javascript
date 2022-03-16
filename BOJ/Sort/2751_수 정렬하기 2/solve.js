30548f6682a1853b28a1125ef86f761c771bbaf3
고동현 <dhjk35@naver.com>
2022-03-16 12:38:06 +0900

    Fail: [백준 Silver 5] 2751번 수 정렬하기 2 - 퀵 정렬 메모리 초과

--- BOJ/Sort/2751_수 정렬하기 2/solve.js
+++ BOJ/Sort/2751_수 정렬하기 2/solve.js
@@ -6,9 +6,36 @@ const [n, ...input] = require("fs")
   .split("\n")
   .map((v) => +v);
 
-function Solution(input) {
-  const answer = input.sort((a, b) => a - b);
+// function Solution(input) {
+//   const answer = input.sort((a, b) => a - b);
+//   console.log(answer.join("\n"));
+// }
+
+// Solution(input);
+
+function Solution(n, input) {
+  const quickSort = (arr) => {
+    if (arr.length < 2) return arr;
+    const pivotIdx = Math.floor(arr.length / 2);
+    const pivot = arr[pivotIdx];
+    arr.splice(pivotIdx, 1);
+
+    const left = [];
+    const right = [];
+
+    for (let idx of arr) {
+      if (idx < pivot) {
+        left.push(idx);
+        continue;
+      }
+      right.push(idx);
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+  };
+
+  const answer = quickSort(input);
   console.log(answer.join("\n"));
 }
 
-Solution(input);
+Solution(n, input);
