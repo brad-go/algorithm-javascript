f19ddd631ca937aa62e8e200231570617f78d906
고동현 <dhjk35@naver.com>
2022-03-16 17:18:55 +0900

    Modify: [백준 Silver 5] 2751번 수 정렬하기 2 - 제출 상태

--- BOJ/Sort/2751_수 정렬하기 2/solve.js
+++ BOJ/Sort/2751_수 정렬하기 2/solve.js
@@ -6,36 +6,40 @@ const [n, ...input] = require("fs")
   .split("\n")
   .map((v) => +v);
 
-// function Solution(input) {
-//   const answer = input.sort((a, b) => a - b);
-//   console.log(answer.join("\n"));
-// }
+// 내 제출
 
-// Solution(input);
-
-function Solution(n, input) {
-  const quickSort = (arr) => {
-    if (arr.length < 2) return arr;
-    const pivotIdx = Math.floor(arr.length / 2);
-    const pivot = arr[pivotIdx];
-    arr.splice(pivotIdx, 1);
-
-    const left = [];
-    const right = [];
-
-    for (let idx of arr) {
-      if (idx < pivot) {
-        left.push(idx);
-        continue;
-      }
-      right.push(idx);
-    }
-
-    return [...quickSort(left), pivot, ...quickSort(right)];
-  };
-
-  const answer = quickSort(input);
+function Solution(input) {
+  const answer = input.sort((a, b) => a - b);
   console.log(answer.join("\n"));
 }
 
-Solution(n, input);
+Solution(input);
+
+// 퀵 정렬 메모리 초과로 실패
+
+// function Solution(n, input) {
+//   const quickSort = (arr) => {
+//     if (arr.length < 2) return arr;
+//     const pivotIdx = Math.floor(arr.length / 2);
+//     const pivot = arr[pivotIdx];
+//     arr.splice(pivotIdx, 1);
+
+//     const left = [];
+//     const right = [];
+
+//     for (let idx of arr) {
+//       if (idx < pivot) {
+//         left.push(idx);
+//         continue;
+//       }
+//       right.push(idx);
+//     }
+
+//     return [...quickSort(left), pivot, ...quickSort(right)];
+//   };
+
+//   const answer = quickSort(input);
+//   console.log(answer.join("\n"));
+// }
+
+// Solution(n, input);
