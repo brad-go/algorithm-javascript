53037856c0804c23bfe22f8f94559d5fdd7d839d
고동현 <dhjk35@naver.com>
2022-03-18 20:11:27 +0900

    Modify: [백준 Silver 5] 1427번 소트인사이드 - 병합 정렬로 구현

--- BOJ/Sort/1427_소트인사이드/solve.js
+++ BOJ/Sort/1427_소트인사이드/solve.js
@@ -47,6 +47,30 @@
 
 // 버블 정렬
 
+// const input = require("fs")
+//   .readFileSync("./input4.txt")
+//   .toString()
+//   .trim()
+//   .split("")
+//   .map((v) => +v);
+
+// function Solution(input) {
+//   for (let i = 0; i < input.length - 1; i++) {
+//     for (let j = input.length - 1; j > i; j--) {
+//       if (input[j] > input[j - 1]) {
+//         const temp = input[j - 1];
+//         input[j - 1] = input[j];
+//         input[j] = temp;
+//       }
+//     }
+//   }
+//   console.log(input.join(""));
+// }
+
+// Solution(input);
+
+// 병합정렬 - 속도는 sort()와 같은나 메모리를 가장 많이 소모
+
 const input = require("fs")
   .readFileSync("./input4.txt")
   .toString()
@@ -55,16 +79,31 @@ const input = require("fs")
   .map((v) => +v);
 
 function Solution(input) {
-  for (let i = 0; i < input.length - 1; i++) {
-    for (let j = input.length - 1; j > i; j--) {
-      if (input[j] > input[j - 1]) {
-        const temp = input[j - 1];
-        input[j - 1] = input[j];
-        input[j] = temp;
+  const merge = (left, right) => {
+    const result = [];
+
+    while (left.length && right.length) {
+      if (left[0] > right[0]) {
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
       }
     }
-  }
-  console.log(input.join(""));
+
+    return [...result, ...left, ...right];
+  };
+
+  const mergeSort = (arr) => {
+    if (arr.length === 1) return arr;
+
+    const midIdx = Math.floor(arr.length / 2);
+    const left = arr.slice(0, midIdx);
+    const right = arr.slice(midIdx);
+
+    return merge(mergeSort(left), mergeSort(right));
+  };
+
+  console.log(mergeSort(input).join(""));
 }
 
 Solution(input);
