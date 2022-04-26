6b4feafccc436cb67681cddfa818674f1d7c0bec
고동현 <dhjk35@naver.com>
2022-04-26 23:05:52 +0900

    Solve: [백준 Silver 2] 11053 가장 긴 증가하는 부분 수열 - 더 깔끔한 풀이 방법 추가

--- BOJ/DynamicProgramming1/11053_가장 긴 증가하는 부분 수열/solve.js
+++ BOJ/DynamicProgramming1/11053_가장 긴 증가하는 부분 수열/solve.js
@@ -5,21 +5,59 @@ const [n, ...input] = require("fs")
   .split(/\s/)
   .map((v) => +v);
 
+// function Solution(n, arr) {
+//   const dp = new Array(n).fill(1);
+
+//   for (let i = 1; i < n; i++) {
+//     const cur = arr[i];
+//     const values = [1];
+
+//     for (let j = 0; j < i; j++) {
+//       const prev = arr[j];
+
+//       if (cur > prev) values.push(dp[j] + 1);
+//     }
+
+//     dp[i] = Math.max(...values);
+//   }
+//   console.log(Math.max(...dp));
+// }
+
+// Solution(n, input);
+
+// Solution 2
+
+// function Solution(n, arr) {
+//   const dp = [];
+
+//   for (let i = 0; i < n; i++) {
+//     dp[i] = 1;
+//     for (let j = 0; j < i; j++) {
+//       console.log(`j, i: ${j},${i}`);
+//       console.log(arr[j], arr[i]);
+//       console.log(dp[i], dp[j]);
+//       if (arr[j] < arr[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+//       console.log(dp);
+//     }
+//     console.log("---");
+//   }
+
+//   console.log(Math.max(...dp));
+// }
+
+// Solution(n, input);
+
+// Solution 2
+
 function Solution(n, arr) {
   const dp = new Array(n).fill(1);
 
   for (let i = 1; i < n; i++) {
-    const cur = arr[i];
-    const values = [1];
-
     for (let j = 0; j < i; j++) {
-      const prev = arr[j];
-
-      if (cur > prev) values.push(dp[j] + 1);
+      if (arr[i] > arr[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
     }
-
-    dp[i] = Math.max(...values);
   }
+
   console.log(Math.max(...dp));
 }
 
