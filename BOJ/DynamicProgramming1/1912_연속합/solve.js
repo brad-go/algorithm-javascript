8a2c51108a5d268df29f5b787512c63ec0eaf569
고동현 <dhjk35@naver.com>
2022-05-03 13:04:16 +0900

    Modify: [백준 Silver 2] 1912 연속합 - 불필요한 연산 제거

--- BOJ/DynamicProgramming1/1912_연속합/solve.js
+++ BOJ/DynamicProgramming1/1912_연속합/solve.js
@@ -1,5 +1,5 @@
 const [n, ...input] = require("fs")
-  .readFileSync("./input.txt")
+  .readFileSync("./input5.txt")
   .toString()
   .trim()
   .split(/\s/)
@@ -10,7 +10,7 @@ function Solution(n, arr) {
   dp[0] = arr[0];
 
   for (let i = 1; i < n; i++) {
-    dp[i] = Math.max(dp[i - 1] + arr[i], arr[i - 1] + arr[i], arr[i]);
+    dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
   }
 
   console.log(Math.max(...dp));
