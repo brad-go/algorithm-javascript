17243cea1b74d04b466fdda1daec40945f1919c0
고동현 <dhjk35@naver.com>
2022-05-03 12:55:47 +0900

    Solve: [백준 Silver 2] 1912 연속합

---
+++ BOJ/DynamicProgramming1/1912_연속합/solve.js
@@ -0,0 +1,19 @@
+const [n, ...input] = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split(/\s/)
+  .map((v) => +v);
+
+function Solution(n, arr) {
+  const dp = new Array(n).fill(0);
+  dp[0] = arr[0];
+
+  for (let i = 1; i < n; i++) {
+    dp[i] = Math.max(dp[i - 1] + arr[i], arr[i - 1] + arr[i], arr[i]);
+  }
+
+  console.log(Math.max(...dp));
+}
+
+Solution(n, input);
