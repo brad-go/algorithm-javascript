40c5fb0d21399399e46f467a2ec2598a3797c9b8
고동현 <dhjk35@naver.com>
2022-05-06 15:52:56 +0900

    Solve: [Programmers Level 1] 로또의 최고 순위와 최저 순위

--- Programmers/Level1/로또의 최고 순위와 최저 순위/solve.js
+++ Programmers/Level1/로또의 최고 순위와 최저 순위/solve.js
@@ -1,18 +1,21 @@
 const [lottos, win_nums] = require("fs")
-  .readFileSync("./input4.txt")
+  .readFileSync("./input5.txt")
   .toString()
   .trim()
   .split("\n")
   .map((arr) => arr.split(" ").map((v) => +v));
 
 function Solution(lottos, win_nums) {
+  // 총 숫자
   const TOTAL_NUMS_COUNT = 6;
+  // 0으로 입력받은 알 수 없는 수
   const UNRECOG_NUMS_COUNT = lottos.filter((num) => num === 0).length;
+  // 맞춘 수에 따른 랭킹을 담은 배열
   const RANK = new Array(TOTAL_NUMS_COUNT)
     .fill()
     .map((_, idx) => idx + 1)
     .reverse();
-
+  // 일치하는 수
   const match_nums = [];
 
   for (let i = 0; i < TOTAL_NUMS_COUNT; i++) {
@@ -22,9 +25,13 @@ function Solution(lottos, win_nums) {
   }
 
   const highest = match_nums.length + UNRECOG_NUMS_COUNT - 1;
-  const lowest = match_nums.length ? match_nums.length - 1 : match_nums.length;
+  const lowest = match_nums.length - 1;
 
-  const answer = [RANK[highest], RANK[lowest]];
+  const answer = [
+    RANK[highest < 0 ? 0 : highest],
+    RANK[lowest < 0 ? 0 : lowest],
+  ];
+
   console.log(answer);
 }
 
