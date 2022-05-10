89fd0a0456239d0a0af223467fd609f29a893327
고동현 <dhjk35@naver.com>
2022-05-11 00:08:22 +0900

    Modify: [백준 Bronze 2] 2798 블랙잭

--- BOJ/BruteForce/2798_블랙잭/problem.md
+++ BOJ/BruteForce/2798_블랙잭/problem.md
@@ -62,5 +62,52 @@ PPCC
 <details><summary><b>문제 풀이</b></summary>
 <div markdown="1">
 
+문제 자체에서 N개의 수 중에서 3장을 뽑는다고 했고, 조합을 떠올릴 수 있었다. 조합을 이용해서 간단하게 풀 수 있었다.
+
+```js
+const [n, m, ...input] = require("fs")
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split(/\s/)
+  .map((v) => +v);
+
+function Solution(n, m, cards) {
+  const MAX_SELECT_COUNT = 3;
+
+  const selected = new Array(n);
+  const selectedCards = new Array(MAX_SELECT_COUNT);
+
+  let answer = 0;
+
+  const makeClose = (answer, selectedCards, m) => {
+    const sum = selectedCards.reduce((acc, cur) => acc + cur, 0);
+    if (sum <= m && sum >= answer) return sum;
+    return answer;
+  };
+
+  const comb = (cnt, start) => {
+    if (cnt === MAX_SELECT_COUNT) {
+      answer = makeClose(answer, selectedCards, m);
+      return;
+    }
+
+    for (let i = start; i < n; i++) {
+      if (selected[i]) continue;
+
+      selected[i] = true;
+      selectedCards[cnt] = cards[i];
+      comb(cnt + 1, i);
+      selected[i] = false;
+    }
+  };
+
+  comb(0, 0);
+  console.log(answer);
+}
+
+Solution(n, m, input);
+```
+
 </div>
 </details>
--- BOJ/BruteForce/2798_블랙잭/solve.js
+++ BOJ/BruteForce/2798_블랙잭/solve.js
@@ -1,5 +1,5 @@
 const [n, m, ...input] = require("fs")
-  .readFileSync("./input2.txt")
+  .readFileSync("./input.txt")
   .toString()
   .trim()
   .split(/\s/)
@@ -9,23 +9,19 @@ function Solution(n, m, cards) {
   const MAX_SELECT_COUNT = 3;
 
   const selected = new Array(n);
-  const nums = new Array(MAX_SELECT_COUNT);
+  const selectedCards = new Array(MAX_SELECT_COUNT);
 
   let answer = 0;
 
-  const makeCloseM = (answer, nums) => {
-    const sum = nums.reduce((acc, cur) => acc + cur, 0);
-
-    if (sum > m) return answer;
-    if (sum === m) return sum;
-
-    if (sum > answer) return sum;
-    else return answer;
+  const makeClose = (answer, selectedCards, m) => {
+    const sum = selectedCards.reduce((acc, cur) => acc + cur, 0);
+    if (sum <= m && sum >= answer) return sum;
+    return answer;
   };
 
   const comb = (cnt, start) => {
     if (cnt === MAX_SELECT_COUNT) {
-      answer = makeCloseM(answer, nums);
+      answer = makeClose(answer, selectedCards, m);
       return;
     }
 
@@ -33,7 +29,7 @@ function Solution(n, m, cards) {
       if (selected[i]) continue;
 
       selected[i] = true;
-      nums[cnt] = cards[i];
+      selectedCards[cnt] = cards[i];
       comb(cnt + 1, i);
       selected[i] = false;
     }
