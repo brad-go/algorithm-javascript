fb2f6668b5be1166b65bcc161716ac25edbd6485
고동현 <dhjk35@naver.com>
2022-05-15 12:54:00 +0900

    Fail: [백준 Silver 2] 24479 알고리즘 수업 - 깊이 우선 탐색 1

--- BOJ/DFS&BFS/DFS1/solve.js
+++ BOJ/DFS&BFS/DFS1/solve.js
@@ -33,31 +33,72 @@ function Solution(input) {
 
   for (let i = 0; i < m; i++) {
     const [vertex1, vertex2] = edges[i];
-    graph.addEdge(vertex1, vertex2, 0);
+    graph.addEdge(vertex1, vertex2, 1);
   }
 
-  const answer = new Array(n).fill(0);
-  let cnt = 1;
+  const dfs = (v, visited) => {
+    visited[v] = true;
+    console.log(v >= n ? 0 : v);
 
-  const dfs = (r) => {
-    const vertices = Object.keys(graph.edges[r]).map((v) => +v);
+    const vertices = Object.keys(graph.edges[v]).map((v) => +v);
 
-    if (!answer[r - 1]) {
-      answer[r - 1] = cnt;
-      cnt++;
-    }
-
-    for (const vertex of vertices) {
-      if (graph.edges[r][vertex]) continue;
-      graph.edges[r][vertex] = 1;
-      graph.edges[vertex][r] = 1;
-      dfs(vertex);
+    for (const n of vertices) {
+      if (!visited[n]) dfs(n, visited);
     }
   };
 
-  dfs(r);
+  const dfsStart = () => {
+    const visited = new Array(n + 1).fill(false);
 
-  answer.forEach((a) => console.log(a));
+    for (let i = 1; i <= n; i++) {
+      if (!visited[i]) dfs(i, visited);
+    }
+  };
+
+  dfsStart();
 }
 
 Solution(input);
+
+// function Solution(input) {
+// const [n, m, r] = input
+//   .shift()
+//   .split(" ")
+//   .map((v) => +v);
+// const edges = input.map((edge) => edge.split(" ").map((v) => +v));
+// const graph = new unDirectedGraph();
+
+// for (let i = 0; i < n; i++) {
+//   graph.addVertex(i + 1);
+// }
+
+// for (let i = 0; i < m; i++) {
+//   const [vertex1, vertex2] = edges[i];
+//   graph.addEdge(vertex1, vertex2, 0);
+// }
+
+//   const answer = new Array(n).fill(0);
+//   let cnt = 1;
+
+//   const dfs = (r) => {
+// const vertices = Object.keys(graph.edges[r]).map((v) => +v);
+
+//     if (!answer[r - 1]) {
+//       answer[r - 1] = cnt;
+//       cnt++;
+//     }
+
+//     for (const vertex of vertices) {
+//       if (graph.edges[r][vertex]) continue;
+//       graph.edges[r][vertex] = 1;
+//       graph.edges[vertex][r] = 1;
+//       dfs(vertex);
+//     }
+//   };
+
+//   dfs(r);
+
+//   answer.forEach((a) => console.log(a));
+// }
+
+// Solution(input);
