358d3406e8588c99b4574c7e0df2c3d18269bb57
고동현 <dhjk35@naver.com>
2022-06-26 17:25:49 +0900

    Modify: [Programmers Level 2] 순위 검색

--- Programmers/Level2/순위 검색/solve.js
+++ Programmers/Level2/순위 검색/solve.js
@@ -17,76 +17,75 @@ const query = [
 ];
 
 function solution(info, query) {
-  const searchConditions = [
+  const options = [
     ["cpp", "java", "python"],
     ["backend", "frontend"],
     ["junior", "senior"],
     ["chicken", "pizza"],
   ];
 
-  let applicants = {};
+  const indexOptions = (parent, depth) => {
+    if (depth === options.length) return parent;
 
-  const indexSearchConditions = (condition, depth) => {
-    if (depth === searchConditions.length) return;
+    options[depth].forEach((option) => {
+      if (depth === options.length - 1) parent[option] = [];
+      else parent[option] = {};
 
-    searchConditions[depth].forEach((subCondition) => {
-      if (depth === searchConditions.length - 1) condition[subCondition] = [];
-      else condition[subCondition] = {};
-      indexSearchConditions(condition[subCondition], depth + 1);
+      indexOptions(parent[option], depth + 1);
     });
   };
 
-  indexSearchConditions(applicants, 0);
+  const getApplicants = (applicants, optionInfo, depth) => {
+    if (depth === optionInfo.length) return applicants;
 
-  info.forEach((applicant) => {
-    const [lang, group, career, food, score] = applicant.split(" ");
-    applicants[lang][group][career][food].push(+score);
-  });
-
-  const getApplicants = (applicatns, condition, depth) => {
-    if (depth === condition.length) return applicatns;
-
-    if (condition[depth] === "-") {
-      // -면 다음 depth로 넘어감
-      return Object.values(applicatns).reduce((acc, cur) => {
-        return acc.concat(getApplicants(cur, condition, depth + 1));
+    if (optionInfo[depth] === "-") {
+      return Object.values(applicants).reduce((acc, cur) => {
+        return [...acc, ...getApplicants(cur, optionInfo, depth + 1)];
       }, []);
     } else {
-      return getApplicants(applicatns[condition[depth]], condition, depth + 1);
+      return getApplicants(applicants[optionInfo[depth]], optionInfo, depth + 1); // prettier-ignore
     }
   };
 
-  // prettier-ignore
-  const binarySearch = (array, target, startIndex = 0, endIndex = array.length - 1) => {
-    while (startIndex <= endIndex) {
-      const middleIndex = Math.floor((startIndex + endIndex) / 2);
+  const binarySearch = (array, target, low = 0, high = array.length - 1) => {
+    if (array.length < 1) return low;
 
-      if (target <= array[middleIndex]) endIndex = middleIndex - 1;
-      else startIndex = middleIndex + 1;
+    while (low <= high) {
+      const mid = Math.floor((low + high) / 2);
+
+      if (target <= array[mid]) high = mid - 1;
+      else low = mid + 1;
     }
 
-    return startIndex;
-  }
+    return low;
+  };
 
-  const conditions = {};
+  const applicantsTree = {};
+  const applicants = {};
   const count = [];
+
+  indexOptions(applicantsTree, 0);
+
+  info.forEach((applicant) => {
+    const [lang, group, career, food, score] = applicant.split(" ");
+
+    applicantsTree[lang][group][career][food].push(+score);
+  });
 
   query.forEach((q) => {
     const [lang, group, career, food, score] = q.replace(/\sand\s/g, " ").split(" "); // prettier-ignore
-    const applicantInfo = [lang, group, career, food];
-    const conditionStr = applicantInfo.join(",");
+    const optionInfo = [lang, group, career, food];
+    const optionIndex = optionInfo.join(",");
 
-    if (conditions[conditionStr] === undefined) {
-      conditions[conditionStr] = getApplicants(applicants, applicantInfo, 0).sort((a, b) => a - b); // prettier-ignore
+    if (applicants[optionIndex] === undefined) {
+      applicants[optionIndex] = getApplicants(applicantsTree, optionInfo, 0).sort((a, b) => a - b); // prettier-ignore
     }
 
-    if (score === "-") count.push(conditions[conditionStr].length);
-    else {
-      const standardScore = Number(score);
-      const qualifiedApplicantCount = binarySearch(conditions[conditionStr], standardScore); // prettier-ignore
+    const applicantsCount =
+      applicants[optionIndex].length -
+      binarySearch(applicants[optionIndex], +score);
 
-      count.push(conditions[conditionStr].length - qualifiedApplicantCount);
-    }
+    count.push(applicantsCount);
   });
 
   return count;
