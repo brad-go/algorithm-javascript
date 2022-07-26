54cc0761dc753e5cebceee01482fe79ea2d62c5d
고동현 <dhjk35@naver.com>
2022-07-26 23:33:44 +0900

    Solve: [백준 Gold 3] 9874 Wormholes

---
+++ BOJ/BruteForce/9874_Wormholes/problem.md
@@ -0,0 +1,7 @@
+# Wormholes - 9874
+
+[문제 링크](https://www.acmicpc.net/problem/9874)
+
+## 문제 풀이
+
+### 풀이 설명
---
+++ BOJ/BruteForce/9874_Wormholes/solve.js
@@ -0,0 +1,82 @@
+// prettier-ignore
+const [n, ...input] = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
+const N = Number(n);
+const wormholeInfo = input.map((line) => line.split(" ").map(Number));
+
+class Warmhole {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.connected = null;
+  }
+
+  connect(index) {
+    this.connected = index;
+  }
+}
+
+function solution(N, wormholeInfo) {
+  const wormholes = wormholeInfo
+    .map(([x, y]) => new Warmhole(x, y))
+    .sort((a, b) => a.x - b.x); // x좌표 기준으로 정렬, +x방향으로 이동하면 x값이 작은 값부터 웜홀을 타기 때문에
+  const visited = new Array(N).fill(false);
+  const answer = connectWormholes(N, wormholes, visited, 0, 0, 0);
+
+  return answer;
+}
+
+const connectWormholes = (N, wormholes, visited, count, index, answer) => {
+  if (count === N) {
+    for (let i = 0; i < N; i++) {
+      if (isCycling(N, wormholes, i)) {
+        answer++;
+
+        return answer++;
+      }
+    }
+  }
+
+  for (let i = index; i < N; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+
+    for (let j = i + 1; j < N; j++) {
+      if (visited[j]) continue;
+      visited[j] = true;
+
+      wormholes[i].connect(j);
+      wormholes[j].connect(i);
+      answer = connectWormholes(N, wormholes, visited, count + 2, i + 1, answer); // prettier-ignore
+      visited[j] = false;
+    }
+    visited[i] = false;
+  }
+
+  return answer;
+};
+
+const isCycling = (N, wormholes, index) => {
+  const visited = new Array(N).fill(false);
+
+  while (true) {
+    if (visited[index]) return true;
+
+    visited[index] = true;
+
+    const x = wormholes[wormholes[index].connected].x;
+    const y = wormholes[wormholes[index].connected].y;
+
+    index = searchNextHole(wormholes, x, y);
+
+    if (index === -1) return false;
+  }
+};
+
+const searchNextHole = (wormholes, x, y) => {
+  for (let i = 0; i < N; i++) {
+    if (wormholes[i].y === y && wormholes[i].x > x) return i;
+  }
+  return -1;
+};
+
+console.log(solution(N, wormholeInfo));
