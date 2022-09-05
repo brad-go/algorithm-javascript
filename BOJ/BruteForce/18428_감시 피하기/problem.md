5e8fd4e808f71f52144b8bb614bf17669d8c12bb
고동현 <dhjk35@naver.com>
2022-09-05 11:45:04 +0900

    Modify: [백준 Gold 5] 18428 감시 피하기 - 코드 개선

--- BOJ/BruteForce/18428_감시 피하기/problem.md
+++ BOJ/BruteForce/18428_감시 피하기/problem.md
@@ -196,3 +196,85 @@ const findStudents = (N, map, i, j, x, y) => {
 
 console.log(solution(N, map));
 ```
+
+### 다시 푼 코드
+
+방문 처리 배열을 사용하지 않으니, 속도나 메모리면에서 더 성능이 좋아졌다. 방문 처리를 하는 것이 별 영향을 끼치지 않을거라고 생각했는데, 오히려 성능을 저하시키고 있었다니..
+
+```js
+// prettier-ignore
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+const N = Number(input[0]);
+const hallway = input.slice(1).map((line) => line.split(" "));
+
+function solution(N, hallway) {
+  const teachers = getPositionsOfTeachers(N, hallway);
+  const result = setUpOstacles(N, hallway, teachers, 0);
+
+  return result ? "YES" : "NO";
+}
+
+const setUpOstacles = (N, hallway, teachers, count) => {
+  if (count === 3) {
+    return !isStudentFound(N, hallway, teachers);
+  }
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (hallway[i][j] !== "X") continue;
+
+      hallway[i][j] = "O";
+      if (setUpOstacles(N, hallway, teachers, count + 1)) return true;
+      hallway[i][j] = "X";
+    }
+  }
+
+  return false;
+};
+
+const isStudentFound = (N, hallway, teachers) => {
+  const DX = [0, 1, 0, -1];
+  const DY = [1, 0, -1, 0];
+
+  for (const { x, y } of teachers) {
+    for (let dir = 0; dir < 4; dir++) {
+      let nx = x + DX[dir];
+      let ny = y + DY[dir];
+
+      const result = lookForStudents(N, hallway, nx, ny, DX[dir], DY[dir]);
+      if (result) return true;
+    }
+  }
+
+  return false;
+};
+
+const getPositionsOfTeachers = (N, hallway) => {
+  const positions = [];
+
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (hallway[i][j] === "T") positions.push({ x: i, y: j });
+    }
+  }
+
+  return positions;
+};
+
+const lookForStudents = (N, hallway, x, y, xDir, yDir) => {
+  if (!isInHallway(N, x, y)) return false;
+
+  const current = hallway[x][y];
+
+  if (current === "S") return true;
+  if (current === "O") return false;
+
+  return lookForStudents(N, hallway, x + xDir, y + yDir, xDir, yDir);
+};
+
+const isInHallway = (N, x, y) => {
+  return 0 <= x && x < N && 0 <= y && y < N;
+};
+
+console.log(solution(N, hallway));
+```
--- BOJ/BruteForce/18428_감시 피하기/solve.js
+++ BOJ/BruteForce/18428_감시 피하기/solve.js
@@ -1,69 +1,75 @@
-const input = require('fs').readFileSync('./input.txt').toString().trim().split('\n'); // prettier-ignore
-const N = Number(input.shift());
-const map = input.map((line) => line.split(" "));
+// prettier-ignore
+const input = require('fs').readFileSync('./input.txt').toString().trim().split('\n');
+const N = Number(input[0]);
+const hallway = input.slice(1).map((line) => line.split(" "));
 
-function solution(N, map) {
-  const visited = Array.from(Array(N), () => Array(N).fill(false));
+function solution(N, hallway) {
+  const teachers = getPositionsOfTeachers(N, hallway);
+  const result = setUpOstacles(N, hallway, teachers, 0);
 
-  return putObstacle(N, map, visited, 0) ? "YES" : "NO";
+  return result ? "YES" : "NO";
 }
 
-const putObstacle = (N, map, visited, count) => {
+const setUpOstacles = (N, hallway, teachers, count) => {
   if (count === 3) {
-    if (!isStudentsFound(N, map)) return true;
-
-    return false;
+    return !isStudentFound(N, hallway, teachers);
   }
 
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < N; j++) {
-      if (map[i][j] !== "X" || visited[i][j]) continue;
+      if (hallway[i][j] !== "X") continue;
 
-      visited[i][j] = true;
-      map[i][j] = "O";
-
-      if (putObstacle(N, map, visited, count + 1)) return true;
-
-      map[i][j] = "X";
-      visited[i][j] = false;
+      hallway[i][j] = "O";
+      if (setUpOstacles(N, hallway, teachers, count + 1)) return true;
+      hallway[i][j] = "X";
     }
   }
 
   return false;
 };
 
-const isStudentsFound = (N, map) => {
+const isStudentFound = (N, hallway, teachers) => {
   const DX = [0, 1, 0, -1];
   const DY = [1, 0, -1, 0];
 
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j < N; j++) {
-      if (map[i][j] !== "T") continue;
+  for (const { x, y } of teachers) {
+    for (let dir = 0; dir < 4; dir++) {
+      let nx = x + DX[dir];
+      let ny = y + DY[dir];
 
-      for (let dir = 0; dir < 4; dir++) {
-        let nx = i + DX[dir];
-        let ny = j + DY[dir];
-        let isFound = findStudents(N, map, nx, ny, DX[dir], DY[dir]);
-
-        if (isFound) return true;
-      }
+      const result = lookForStudents(N, hallway, nx, ny, DX[dir], DY[dir]);
+      if (result) return true;
     }
   }
 
   return false;
 };
 
-const findStudents = (N, map, i, j, x, y) => {
-  if (0 <= i && i < N && 0 <= j && j < N) {
-    const current = map[i][j];
+const getPositionsOfTeachers = (N, hallway) => {
+  const positions = [];
 
-    if (current === "S") return true;
-    if (current === "O") return false;
-
-    return findStudents(N, map, i + x, j + y, x, y);
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      if (hallway[i][j] === "T") positions.push({ x: i, y: j });
+    }
   }
 
-  return false;
+  return positions;
 };
 
-console.log(solution(N, map));
+const lookForStudents = (N, hallway, x, y, xDir, yDir) => {
+  if (!isInHallway(N, x, y)) return false;
+
+  const current = hallway[x][y];
+
+  if (current === "S") return true;
+  if (current === "O") return false;
+
+  return lookForStudents(N, hallway, x + xDir, y + yDir, xDir, yDir);
+};
+
+const isInHallway = (N, x, y) => {
+  return 0 <= x && x < N && 0 <= y && y < N;
+};
+
+console.log(solution(N, hallway));
